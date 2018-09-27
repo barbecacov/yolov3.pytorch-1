95a92acace84c53acccac7f79467239064eb4bd7
Ecr23 <370095872@qq.com>
2018-09-27 11:37:11 +0800

    remove redundant function

--- src/utils.py
+++ src/utils.py
@@ -38,18 +38,6 @@ def parse_cfg(cfgfile):
   return blocks
 
 
-def get_test_input():
-  """Generate test image"""
-  import cv2
-  img = cv2.imread("../assets/test.png")
-  img = cv2.resize(img, (416, 416))          # resize to the input dimension
-  img_ = img[:, :, ::-1].transpose((2, 0, 1))  # BGR -> RGB | H X W C -> C X H X
-  img_ = img_[np.newaxis, :, :, :]/255.0       # Add a channel at 0 (for batch) | Normalise
-  img_ = torch.from_numpy(img_).float()     # Convert to float
-  img_ = Variable(img_)                     # Convert to Variable
-  return img_
-
-
 def transform_coord(bbox):
   """Transform bbox coordinates
     |---------|           (x1,y1) *---------|
